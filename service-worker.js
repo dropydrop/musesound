18cbea50ea3bbf92d625debc5d8316b23a2973fc
dropydrop <register35770@gmail.com>
2026-08-13 16:36:13 +0200

    Bump PWA version to 1.2.6

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = '1.2.5'; // CHANGER CE NUMÉRO À CHAQUE NOUVELLE MISE À JOUR POUR LE PWA
+const VERSION = '1.2.6'; // CHANGER CE NUMÉRO À CHAQUE NOUVELLE MISE À JOUR POUR LE PWA
 const CACHE_NAME = 'musesound-v-' + Date.now();
 
 const ASSETS = [
